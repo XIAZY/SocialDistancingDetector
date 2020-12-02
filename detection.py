7848b43fa75b5afb9379cee7bcaac2115efbd270
XIAZY <i@mechanus.io>
2020-12-02 22:45:48 +0800

    PEP-8

--- detection.py
+++ detection.py
@@ -5,78 +5,86 @@ import configparser
 
 # adapted from https://github.com/opencv/opencv/blob/master/samples/dnn/object_detection.py
 
+
 class Detector:
-  def init_model(self, config_file, weights_file):
-    self.net = cv.dnn.readNetFromDarknet(config_file, weights_file)
-    self.net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+    def init_model(self, config_file, weights_file):
+        self.net = cv.dnn.readNetFromDarknet(config_file, weights_file)
+        self.net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
 
-    config = configparser.ConfigParser(strict=False)
-    config.read(config_file)
+        config = configparser.ConfigParser(strict=False)
+        config.read(config_file)
 
-    self.inputShape = (int(config['net']['width']), int(config['net']['height']))
-  
-  def load_classes(self, classes_file):
-    self.classes = open(classes_file).read().strip().split('\n')
+        self.inputShape = (
+            int(config['net']['width']), int(config['net']['height']))
 
-  def detect(self, img, conf_thresh, NMS_thresh):
-    blob = cv.dnn.blobFromImage(img, 1/255.0, self.inputShape, swapRB=True, crop=False)
+    def load_classes(self, classes_file):
+        self.classes = open(classes_file).read().strip().split('\n')
 
-    self.net.setInput(blob)
-    outs = self.net.forward(self.net.getUnconnectedOutLayersNames())
+    def detect(self, img, conf_thresh, NMS_thresh):
+        blob = cv.dnn.blobFromImage(
+            img, 1/255.0, self.inputShape, swapRB=True, crop=False)
 
-    h, w = img.shape[:2]
+        self.net.setInput(blob)
+        outs = self.net.forward(self.net.getUnconnectedOutLayersNames())
 
-    classes = []
-    confs = []
-    boxes = []
+        h, w = img.shape[:2]
 
-    for out in outs:
-      for detection in out:
-          scores = detection[5:]
-          classId = np.argmax(scores)
-          conf = scores[classId]
+        classes = []
+        confs = []
+        boxes = []
 
-          if conf > conf_thresh:
-              center_x = int(detection[0] * w)
-              center_y = int(detection[1] * h)
-              width = int(detection[2] * w)
-              height = int(detection[3] * h)
-              left = int(center_x - width / 2)
-              top = int(center_y - height / 2)
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                classId = np.argmax(scores)
+                conf = scores[classId]
 
-              classes.append(self.classes[classId])
-              confs.append(float(conf))
-              boxes.append([left, top, width, height])
-    
-    nms_indices = cv.dnn.NMSBoxes(boxes, confs, conf_thresh, NMS_thresh).flatten()
+                if conf > conf_thresh:
+                    center_x = int(detection[0] * w)
+                    center_y = int(detection[1] * h)
+                    width = int(detection[2] * w)
+                    height = int(detection[3] * h)
+                    left = int(center_x - width / 2)
+                    top = int(center_y - height / 2)
 
-    classes = [classes[i] for i in nms_indices]
-    confs = [confs[i] for i in nms_indices]
-    boxes = [boxes[i] for i in nms_indices]
+                    classes.append(self.classes[classId])
+                    confs.append(float(conf))
+                    boxes.append([left, top, width, height])
 
-    return zip(classes, confs, boxes)
+        nms_indices = cv.dnn.NMSBoxes(
+            boxes, confs, conf_thresh, NMS_thresh).flatten()
+
+        classes = [classes[i] for i in nms_indices]
+        confs = [confs[i] for i in nms_indices]
+        boxes = [boxes[i] for i in nms_indices]
+
+        return zip(classes, confs, boxes)
+
 
 def draw_boxes(img, persons):
-  for person in persons:
-    left, top, w, h = person[2]
-    cv.rectangle(img, (left, top), (left + w, top + h), (255, 0, 0), 1)
+    for person in persons:
+        left, top, w, h = person[2]
+        cv.rectangle(img, (left, top), (left + w, top + h), (255, 0, 0), 1)
 
-  cv.imshow('boxes', img)
-  cv.waitKey()
+    cv.imshow('boxes', img)
+    cv.waitKey()
+
 
 def get_persons(img):
-  detector = Detector()
-  detector.init_model('yolo/yolov3.cfg', 'yolo/yolov3.weights')
-  detector.load_classes('yolo/coco.names')
-  detected = detector.detect(img, 0.5, 0.5)
+    detector = Detector()
+    detector.init_model('yolo/yolov3.cfg', 'yolo/yolov3.weights')
+    detector.load_classes('yolo/coco.names')
+    detected = detector.detect(img, 0.5, 0.5)
 
-  return filter(lambda obj: obj[0]=='person', detected)
+    return filter(lambda obj: obj[0] == 'person', detected)
+
 
 def main():
-  img = cv.imread('images/younge.jpg')
+    img = cv.imread('images/younge.jpg')
 
-  persons = get_persons(img)
-  draw_boxes(img, persons)
+    persons = get_persons(img)
+    draw_boxes(img, persons)
+
 
 if __name__ == '__main__':
-  main()
+    main()
