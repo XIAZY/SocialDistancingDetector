7f9f3291f487c1461d897b0511be12bc0a58ae1a
Tianze Xu <tianzexu00@gmail.com>
2020-12-09 20:56:27 +0800

    Show violators for bird's eye view method

--- point_input.py
+++ point_input.py
@@ -3,24 +3,21 @@ import cv2 as cv
 
 # adapted from https://docs.opencv.org/master/db/d5b/tutorial_py_mouse_handling.html
 
-class Box:
+class PointInput:
   def draw_circle(self, event, x, y, flags, param):
       if event == cv.EVENT_LBUTTONDBLCLK:
         self.points.append((x, y))
         cv.circle(self.img, (x, y), 10, (255, 0, 0), -1)
 
-  def box(self, img):
+  def n_points(self, n, img):
     # return four points drawn by the user, along with the modified image
-
-    print('please draw four points that would look like a square from birdseye view')
-
     self.points = []
     self.img = img.copy()
     
     cv.namedWindow('image')
     cv.setMouseCallback('image', self.draw_circle)
 
-    while len(self.points) < 4:
+    while len(self.points) < n:
       cv.imshow('image', self.img)
       k = cv.waitKey(20) & 0xFF
     
